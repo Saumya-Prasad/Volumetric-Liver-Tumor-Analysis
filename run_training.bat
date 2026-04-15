a12ea1b3b757ab68d030b0ad758c4cf989fc56fa
Saumya Prasad <saumyaprasad2004@gmail.com>
2026-04-16 04:08:15 +0530

    num of epoches chaged

--- run_training.bat
+++ run_training.bat
@@ -2,21 +2,21 @@
 set "PYTHON_EXE=C:\Users\Saumya_Shambhavi\anaconda3\envs\liver_tumor\python.exe"
 
 echo Running Conv AE...
-%PYTHON_EXE% train.py --model conv_ae --epochs 5 --batch 16
+%PYTHON_EXE% train.py --model conv_ae --epochs 15 --batch 16
 
 echo Running AE Flow...
-%PYTHON_EXE% train.py --model ae_flow --epochs 5 --batch 16
+%PYTHON_EXE% train.py --model ae_flow --epochs 15 --batch 16
 
 echo Running Masked AE...
-%PYTHON_EXE% train.py --model masked_ae --epochs 5 --batch 8
+%PYTHON_EXE% train.py --model masked_ae --epochs 15 --batch 8
 
 echo Running CCB AAE...
-%PYTHON_EXE% train.py --model ccb_aae --epochs 5 --batch 16
+%PYTHON_EXE% train.py --model ccb_aae --epochs 15 --batch 16
 
 echo Running QFormer...
-%PYTHON_EXE% train.py --model qformer --epochs 5 --batch 8
+%PYTHON_EXE% train.py --model qformer --epochs 15 --batch 8
 
 echo Running Ensemble...
-%PYTHON_EXE% train.py --model ensemble --epochs 5 --batch 16
+%PYTHON_EXE% train.py --model ensemble --epochs 15 --batch 16
 
 echo All models finished!
